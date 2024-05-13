2b70dc64c06979ca2f18c7ed7165d2e7f8d3a772
Rajeev MYLAPALLI <rajeev.msn@gmail.com>
2024-05-13 16:22:01 +0200

    credentials.h example

---
+++ credntials_Example.h
@@ -0,0 +1,12 @@
+// This file is an example of the credentials.h file that you need to create in order to run the code.
+// Rename this to credentials.h and fill in the SSID_NAME, SSID_PW and TOKEN with your own values.
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+const char* SSID_NAME = "YOUR_SSID_NAME";
+const char* SSID_PW = "YOUR_SSID_PW";
+
+//Refer to the README.md file to get the access token
+const char* TOKEN = "your_token_here";
+
+#endif
